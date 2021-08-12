30a46483ee76a51488ff6eda3061c8341ec6d2d3
Zhenqiu Liu <45047002+zliu3@users.noreply.github.com>
2021-08-12 03:37:35 -0400

    Delete nsampdriv6.m

--- nsampdriv6.m
+++
@@ -1,7 +0,0 @@
-load fisheriris
-X = meas(51:end,:); % versicolor and virginica
-X = X(:, [3,4]);
-
-y = strcmp('versicolor',species(51:end));
-mdl = fitglm(X,y,'linear','distr','binomial');
-[w, s2] = logisticregp(X, y, 0.1);
